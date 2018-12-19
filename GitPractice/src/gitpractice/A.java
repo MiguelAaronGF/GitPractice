908f203f3b8fd769b7e1c1928665db7b783aba4d
MiguelAaronGF <33825996+MiguelAaronGF@users.noreply.github.com>
2018-12-19 10:55:12 +0100

    First upload within a branch
    Still trying things

---
+++ GitPractice/src/gitpractice/A.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitpractice;
+
+/**
+ *
+ * @author dawmi
+ */
+public class A {
+    public static void hello(){
+        System.out.print("Hello");
+    }
+}
